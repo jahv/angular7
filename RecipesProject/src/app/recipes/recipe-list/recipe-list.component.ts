7a9f38b4346d39952ce5468f4510b0522eee8ef2
José Antonio Hernández Vázquez <jahv.89@gmail.com>
2019-07-15 16:12:48 -0500

    iterating on array to display recipes(not used the recipe-item yet)

--- RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is  test', 'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2019/03/04141012/lime-roasted-salmon-skillet-square-500x500.jpg')
+  ];
 
   constructor() { }
 
